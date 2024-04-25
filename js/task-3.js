bf3498290db55b8789219927b34398ce402bacf8
mish3mish <mishakostiuchenko@gmail.com>
2024-04-24 23:07:06 -0400

    1st try

---
+++ js/task-3.js
@@ -0,0 +1,51 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+
+    changeUsername(newName) {
+        this.username = newName;
+
+    },
+
+    updatePlayTime(hours) {
+        this.playTime += hours;
+
+    },
+};
+
+
+
+
+
+
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+// const player = {
+//     username: "Alice",
+//     score: 1500,
+
+// changeUsername(newName) {
+//     this.username = newName;
+// },
+
+// updateScore(points) {
+//     this.score += points;
+// },
+
+// getInfo() {
+//     return `${this.username} has a score of ${this.score}`;
+// },
+// };
